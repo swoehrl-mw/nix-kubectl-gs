de35d1ad443e370d953cac5f289ebd30d5aa3be8
Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>
2024-06-22 14:33:24 +0200

    Initial release with v2.57.0

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# nix-kubectl-gs
+# Nix Flake for kubectl-gs
+
+This repository contains a very basic [Nix Flake](https://nixos.wiki/wiki/Flakes) providing the [Giantswarm Kubectl plugin](https://github.com/giantswarm/kubectl-gs) as a package. It is intended to be added to dev shells.
+
+## Usage
+
+To reference this flake use the following format: `github:swoehrl-mw/nix-kubectl-gs/v<version>`. If you just want the latest version and don't care about version pinning, remove the `/v<version>` part.
+
+Example: `github:swoehrl-mw/nix-kubectl-gs/v2.57.0`
+
+To use the package with [devbox](https://www.jetify.com/devbox/docs/), simply add the reference to your `devbox.json` `packages`:
+
+```json
+{
+    "packages": [
+        "kubectl@latest",
+        "github:swoehrl-mw/nix-kubectl-gs/v2.57.0"
+    ]
+}
+```
+
+To add the package to a classic [nix-shell](https://nix.dev/tutorials/first-steps/declarative-shell), use the following snippet in your `shell.nix`:
+
+```nix
+pkgs.mkShellNoCC {
+  packages = [
+    pkgs.kubectl
+    (builtins.getFlake "github:swoehrl-mw/nix-kubectl-gs/v<version>").packages.${builtins.currentSystem}.kubectl-gs
+  ];
+}
+```
+
+## Development
+
+Steps to update the Flake to a new version:
+
+1. Change the version in `calc_hashes.sh` and run the script
+2. Edit `flake.nix`:
+   1. Paste the output of the script into the `source_hashes` block
+   2. Change the `pkg_version`
+3. Commit and push the changes
+4. Tag the commit with the new version
---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  outputs = { self, nixpkgs }:
+    let
+      # System types to support.
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let 
+          pkgs = nixpkgsFor.${system};
+          pkg_version = "2.57.0";
+          # Map nix system names to download names for kubectl-gs releases
+          name_mapping = {
+            "x86_64-linux" = "linux-amd64";
+            "aarch64-linux" = "linux-arm64";
+            "x86_64-darwin" = "darwin-amd64";
+            "aarch64-darwin" = "darwin-arm64";
+          };
+          # Hashes for the different architectures, calculated using ./calc_hashes.sh
+          source_hashes = {
+            "linux-amd64" = "UNUxL3g6nzvQlxuhWBrwygrO8ZEslxCWXf1ofOe9zoo=";
+            "linux-arm64" = "l9yvjFaHcTK+boOYcu36NF1+W9ih5CqfL0vbOsmXiBo=";
+            "darwin-amd64" = "q6rxPlR0YU50/8eV6fWoRK/nD8P70s4vgdqoba8D0eM=";
+            "darwin-arm64" = "V4uSVTkyualxRs6uesEzTA37maqzg7IFtF563kOSFLM=";
+          };
+        in {
+          kubectl-gs = pkgs.stdenv.mkDerivation rec {
+            pname = "kubectl-gs";
+            version = "${pkg_version}";
+            src = pkgs.fetchurl {
+              url =
+                "https://github.com/giantswarm/kubectl-gs/releases/download/v${pkg_version}/kubectl-gs-v${pkg_version}-${name_mapping.${system}}.tar.gz";
+              sha256 = "${source_hashes.${name_mapping.${system}}}";
+            };
+
+            dontConfigure = true;
+            dontBuild = true;
+            installPhase = ''
+              mkdir -p $out/bin
+              cp kubectl-gs $out/bin
+            '';
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.kubectl-gs);
+    };
+}
